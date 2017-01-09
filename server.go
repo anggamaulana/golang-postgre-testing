2760c5969e12498a981f54a24844a79d570fdeca
angga <angga.mn@gmail.com>
2017-01-09 19:19:55 +0700

    first push

---
+++ server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	// "bytes"
+	"fmt"
+	"io"
+	// "io/ioutil"
+	"crypto/md5"
+	"log"
+	// "mime/multipart"
+	"net/http"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/hello2", HelloServer)
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+// upload logic
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
